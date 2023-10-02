0daeb05183e27c27abe48b28957fca61b8a5761e
ildar <ildar.bilalov@list.ru>
2023-10-02 18:06:01 +0300

    asd

--- Quest/src/test/java/VariantTest.java
+++ Quest/src/test/java/VariantTest.java
@@ -1,8 +1,5 @@
 import org.junit.jupiter.api.Test;
-import ru.javarush.bilalov.quest.entity.Answer;
 import ru.javarush.bilalov.quest.entity.Variant;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
